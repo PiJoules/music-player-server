09abe1fa4d389acea23e0e5171f09bb08a6fc1c6
pijoules <lchan1994@yahoo.com>
2016-03-05 21:06:29 -0500

    can upload mp3s

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # music-player-server
+
+
+
+### Saving an image into an mp3
+http://tuxpool.blogspot.com/2013/02/how-to-store-images-in-mp3-files-using.html
--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from __future__ import print_function
 import os
 import eyed3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from uuid import uuid4
 
 
@@ -23,11 +23,77 @@ app.jinja_env.filters["path_join"] = os.path.join
 WORKING_DIR = "static/audio"
 
 
+class Song(object):
+    """Container for song info."""
+
+    __slots__ = ("title", "artist", "album", "cover", "src", "base_src")
+
+    def __init__(self, src, **kwargs):
+        for attr in self.__slots__:
+            val = kwargs.get(attr, None)
+            setattr(self, attr, val)
+        self.src = src
+        self.base_src = os.path.basename(src)
+
+    @classmethod
+    def from_path(cls, path):
+        """
+        Create a song obj from the path to the file.
+
+        This path must be the actual path of the computer this server is
+        running on, not the relative flask server. '/' represents the OS root,
+        not the flask root.
+        """
+        tag = eyed3.load(path).tag
+        if tag.images:
+            cover = tag.images[0]
+        else:
+            cover = None
+        return cls(path, title=tag.title, artist=tag.artist, album=tag.album,
+                   cover=cover)
+
+    @property
+    def base64_cover(self):
+        """base64 encoded cover image data."""
+        cover = self.cover
+        if cover:
+            return self.cover.image_data.encode("base64")
+        return None
+
+    def __str__(self):
+        return str({attr: getattr(self, attr, None)
+                    for attr in self.__slots__})
+
+
 @app.route("/")
 def index_route():
+    audio_files = os.listdir(WORKING_DIR)
+    songs = [Song.from_path(os.path.join(WORKING_DIR, f)) for f in audio_files]
     return render_template("index.html",
-                           title="Home",
-                           audio_files=os.listdir(WORKING_DIR))
+                           songs=songs)
+
+
+@app.route("/<filename>/edit", methods=["GET", "POST"])
+def edit_song_route(filename):
+    """Route for editing song info."""
+    relpath = os.path.join(WORKING_DIR, filename)
+    if request.method == "GET":
+        song = Song.from_path(relpath)
+        print(song)
+        return render_template("edit.html", song=song)
+
+    # POST
+    print(request.form)
+    print(request.files)
+    cover = request.files.get("cover", None)
+    tag = eyed3.load(relpath).tag
+    tag.artist = request.form["artist"]
+    tag.album = request.form["album"]
+    tag.title = request.form["title"]
+    if cover:
+        tag.images.set(3, cover.read(), cover.mimetype)
+    tag.save()
+    return ""
 
 
 @app.route("/<filename>")
@@ -41,6 +107,7 @@ def song_route(filename):
     print(audio_file.tag.album_artist)
     print(audio_file.tag.title)
     print(list(audio_file.tag.images))
+    print(audio_file.info.time_secs)
 
     return render_template("play.html",
                            title=filename,
